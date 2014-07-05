a80301a0514abea752dbf7858baddb41203a1a8e
ranjithkm <ranjith213kumar@gmail.com>
2014-07-05 18:22:46 +0530

    first version of reverse.pl

--- reverse.pl
+++ reverse.pl
@@ -5,8 +5,12 @@
  use Data::Dumper;
 
  my @given= ( 1, 2, 3, 4, 5, 6 );
- say Dumper "@given";
- my $length = $#given + 1;
+ my $length = $#given;
+ say "$length";
  my @reverse;
  foreach my $element(@given)  {
-        
+        $reverse[$length] = $element;
+        $length--;
+ }
+ say Dumper \@given;
+ say Dumper \@reverse;          

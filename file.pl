6b35ee27c75e00dc8a3606844abc86669d90e647
ranjithkm <ranjith213kumar@gmail.com>
2014-07-08 13:12:06 +0530

    edited file.pl

--- file.pl
+++ file.pl
@@ -6,19 +6,27 @@ use feature ':5.12';
 {
 open(my $fh, "<", "tom.txt")
 or die "cannot open > tom.txt: $!";
-my $math = 60;
-my $science = 70;
-my $average = (($math+$science)/2);
-say "tom's $average\%";
+#my $math = 60;
+#my $science = 70;
+#my $average = (($math+$science)/2);
+#say "tom's $average\%";
+while (my $row = <$fh>) {
+    chomp $row;
+    print "$row\n"
+}
 close $fh;
 }
 
 {
 open(my $fh, "<", "bob.txt")
 or die "cannot open > bob.txt: $!";
-my $math = 76;
-my $science = 90;
-my $average = (($math+$science)/2);
-say "bob's $average\%";
+#my $math = 76;
+#my $science = 90;
+#my $average = (($math+$science)/2);
+#say "bob's $average\%";
+while (my $row = <$fh>) {
+    chomp $row;
+    print "$row\n"
+}
 close $fh;
 }
